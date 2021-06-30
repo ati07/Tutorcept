2308f893099ac4f126998dcd328f9d4b4b40a7a3
unknown <atiurrahman.ansari@gmail.com>
2021-06-30 13:14:10 +0530

    7th commit

--- component/CurriculumSlider.js
+++ component/CurriculumSlider.js
@@ -1,9 +1,77 @@
-const CurriculumSlider = () => {
-    return (
-        <div className='c_slider flex flex-col align-center bg-[#0a2540] w-full h-[700px] mt-[-210px]'>
-            
-        </div>
-    )
-}
+import React, { useRef, useState } from "react";
+// Import Swiper React components
+import { Swiper, SwiperSlide } from "swiper/react";
 
-export default CurriculumSlider
+// Import Swiper styles
+import "swiper/swiper.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import "swiper/components/navigation/navigation.min.css";
+
+// import "./styles.css";
+
+// import Swiper core and required modules
+import SwiperCore, { Pagination, Navigation } from "swiper/core";
+
+// install Swiper modules
+SwiperCore.use([Pagination, Navigation]);
+
+const CurriculumSlider = () => {
+  return (
+    <div className="c_slider flex flex-col align-center bg-[#0a2540] w-full h-[1000px] mt-[-150px]">
+      <div className="flex flex-col justify-center items-center mt-[170px] h-[600px]">
+        <div>
+          <h1 className="text-4xl text-white uppercase">Indian Curriculum</h1>
+        </div>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={20}
+          slidesPerGroup={4}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+        <div className='mt-10'>
+          <h1 className="text-4xl text-white uppercase">USA Curriculum</h1>
+        </div>
+        <Swiper
+          slidesPerView={4}
+          spaceBetween={20}
+          slidesPerGroup={4}
+          loop={true}
+          loopFillGroupWithBlank={true}
+          pagination={{
+            clickable: true,
+          }}
+          navigation={true}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default CurriculumSlider;
--- component/WhyUs.js
+++ component/WhyUs.js
@@ -1,27 +1,17 @@
 import {Button} from '@material-ui/core'
 function WhyUs() {
   return (
-    <div className="whyus w-full bg-[#becdda] border-2 h-[900px] relative top-[-96px]">
-      <div className="flex justify-center w-full h-auto mt-20 text-3xl font-semibold align-center">
-        <h1 className="text-4xl font-bold">Why use Tutorcept?</h1>
+    <div className="whyus flex flex-col items-center justify-center w-full bg-[#becdda] border-2 h-[1300px] mt-[-200px]">
+      <div className='flex flex-col relative top-[-130px] items-center'>
+      <div className="animate-bounce advantage flex w-[400px] pt-4 justify-center h-[200px] font-semibold bg-[#2c5698] relative top-[380px]">
+        <h1 className="text-3xl font-bold text-center text-white uppercase">Advantage <br/>of <br/>Tutorcept</h1>
       </div>
-      <div className="w-full grid grid-cols-8 gap-6 justify-center mt-10 item-center lg:px-[150px]">
-        <div className="w-full col-span-4 bg-white h-80">
-          <div className="grid w-full grid-cols-12 p-4">
-            <div className="w-full col-span-3">LOGO</div>
-            <div className="w-full col-span-9">
-              <h1 className='text-2xl font-medium uppercase opacity-70'>On-demand tutoring</h1>
-              <p>
-                Connect with an online tutor in less than 30 seconds, 24/7. It
-                doesn’t matter if you want help with a single problem or you
-                need a 3-hour lesson.
-              </p>
-            </div>
-          </div>
+      <div className="w-full grid grid-cols-8 gap-10 gap-y-20 justify-center item-center lg:px-[150px]">
+        <div className="w-full col-span-3 bg-white shadow-lg rounded-xl h-50 gap-y-20">
           <div className="grid w-full grid-cols-12 p-4">
           <div className="w-full col-span-3">LOGO</div>
             <div className="w-full col-span-9">
-              <h1 className='text-2xl font-medium uppercase opacity-70'>On-demand tutoring</h1>
+              <h1 className='text-2xl font-medium uppercase opacity-70'>Quality Tutors</h1>
               <p>
                 Connect with an online tutor in less than 30 seconds, 24/7. It
                 doesn’t matter if you want help with a single problem or you
@@ -30,12 +20,39 @@ function WhyUs() {
             </div>
           </div>
         </div>
-        <div className="w-full col-span-4 bg-white h-80">
-        <div className="w-full col-span-4 bg-white h-80">
+        <div className="w-full col-span-3 col-start-6 bg-white shadow-lg rounded-xl h-50 gap-y-20">
+          <div className="grid w-full grid-cols-12 p-4">
+            <div className="w-full col-span-3">LOGO</div>
+              <div className="w-full col-span-9">
+                <h1 className='text-2xl font-medium uppercase opacity-70'>Affordable Price</h1>
+                <p>
+                Connect with an online tutor in less than 30 seconds, 24/7. It
+                doesn’t matter if you want help with a single problem or you
+                need a 3-hour lesson.
+                </p>
+            </div>
+          </div>
+      
+        </div>
+        <div className="w-full col-span-3 ml-[77px] col-start-1 bg-white shadow-lg rounded-xl h-50 gap-y-20">
+          <div className="grid w-full grid-cols-12 p-4">
+            <div className="w-full col-span-3">LOGO</div>
+              <div className="w-full col-span-9">
+                <h1 className='text-2xl font-medium uppercase opacity-70'>Learn Anytime</h1>
+                <p>
+                Connect with an online tutor in less than 30 seconds, 24/7. It
+                doesn’t matter if you want help with a single problem or you
+                need a 3-hour lesson.
+                </p>
+            </div>
+          </div>
+      
+        </div>
+        <div className="w-full col-span-3 ml-[-77px] col-start-6 bg-white shadow-lg rounded-xl h-50 gap-y-20">
           <div className="grid w-full grid-cols-12 p-4">
             <div className="w-full col-span-3">LOGO</div>
             <div className="w-full col-span-9">
-              <h1 className='text-2xl font-medium uppercase opacity-70'>On-demand tutoring</h1>
+              <h1 className='text-2xl font-medium uppercase opacity-70'>Expert&apos;s Resources</h1>
               <p>
                 Connect with an online tutor in less than 30 seconds, 24/7. It
                 doesn’t matter if you want help with a single problem or you
@@ -43,10 +60,13 @@ function WhyUs() {
               </p>
             </div>
           </div>
+      
+        </div>
+        <div className="w-full col-span-4 col-start-3 bg-white shadow-lg h-50 rounded-xl gap-y-20">
           <div className="grid w-full grid-cols-12 p-4">
-          <div className="w-full col-span-3">LOGO</div>
+            <div className="w-full col-span-3">LOGO</div>
             <div className="w-full col-span-9">
-              <h1 className='text-2xl font-medium uppercase opacity-70'>On-demand tutoring</h1>
+              <h1 className='text-2xl font-medium uppercase opacity-70'>Break The language Barriers</h1>
               <p>
                 Connect with an online tutor in less than 30 seconds, 24/7. It
                 doesn’t matter if you want help with a single problem or you
@@ -54,11 +74,13 @@ function WhyUs() {
               </p>
             </div>
           </div>
+      
         </div>
-        </div>
+      
+      
       </div>
-    
-        <div className='h-[50px] flex items-center justify-around w-full h-auto mt-20'>
+
+        <div className='h-[50px] flex items-center justify-around w-full mt-10'>
             <div className='flex px-10 text-white text-center justify-center w-56 h-[40px] uppercase bg-blue-600 rounded-lg'>
                 <Button className='w-full text-white'>Request A Tutor</Button>
             </div>
@@ -66,6 +88,7 @@ function WhyUs() {
                 <Button className='w-full '> Request a Demo</Button>
             </div>
 
+        </div>
         </div>
     </div>
   );
