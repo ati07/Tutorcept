6c0e5eca81d238d840f91d8357402bd35d75fb4d
unknown <atiurrahman.ansari@gmail.com>
2021-07-09 12:27:20 +0530

    23th commit

--- pages/faqs.js
+++ pages/faqs.js
@@ -11,28 +11,9 @@ import AccordionDetails from "@material-ui/core/AccordionDetails";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
-
+import datajson from '../data.json'
 function Faqs() {
-  const data = [
-    {
-      qid: 1,
-      question: "Is your tutoring 1:1 or with a group?",
-      answer:
-        "Yes, Tutorcept provides both 1:1 as well as tutoring of a small group (1:10). Our tutors are trained to deliver lessons for an individual’s need and a group too.",
-    },
-    {
-      qid: 2,
-      question: "Who are the tutors?",
-      answer:
-        "Our tutors come from diverse backgrounds including current and former teachers, experienced professionals from the medical, engineering, and humanities field. We recruit tutors from top colleges and universities who are experts in their respective academic subjects and passionate about helping students achieve their goals.",
-    },
-    {
-      qid: 3,
-      question: "Can I learn in any other language besides English?",
-      answer:
-        "We offer tutoring in English as students find it the most comfortable language. Besides, we also have the option of Tutoring in Hindi for the students following Indian Curriculums. Very soon we shall be expanding our tutoring services in several other languages too.",
-    },
-  ];
+  // console.log("data",datajson)
   const useStyles = makeStyles((theme) => ({
     root: {
       width: "80%",
@@ -68,7 +49,7 @@ function Faqs() {
       <WhiteHeader />
 
       <div className="w-full h-auto flex flex-col mt-[150px] px-52 py-8">
-        <div className="text-center pb-10">
+        <div className="pb-10 text-center">
           <h1 className="text-5xl text-[#1e56a0] font-bold pb-7">
             Frequently Asked Questions
           </h1>
@@ -77,7 +58,7 @@ function Faqs() {
             answer to your question.
           </p>
         </div>
-        {data.map((item, i) => (
+        {datajson.map((item, i) => (
           <Accordion
           key={i}
             expanded={expanded === `panel${i + 1}`}
@@ -94,7 +75,10 @@ function Faqs() {
               {/* <Typography >I am an accordion</Typography> */}
             </AccordionSummary>
             <AccordionDetails>
-              <Typography >{item["answer"]}</Typography>
+              <Typography dangerouslySetInnerHTML={{__html: item["answer"]}}>
+                {}
+                
+              </Typography>
             </AccordionDetails>
           </Accordion>
   ))}
